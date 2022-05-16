a3bf7663a83b919510d245b01f0c6c25faf24923
Tarek Chmaitelly <tarekchmaitelly@gmail.com>
2022-05-16 20:29:34 +0300

    frontend integration

--- todo-app/server/index.js
+++ todo-app/server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose= require ('mongoose');
 const app = express();
 const bodyParser = require ('body-parser');
+var cors = require('cors')
 
 mongoose.connect("mongodb+srv://tarekchm:tarek123@crud.ofcjm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
     useNewUrlParser: true,
@@ -13,6 +14,14 @@ connection.connectToServer(function (err) {
     app.use('/apis',apis);
     
 });
+
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
+
+  app.use(cors())
+   
+  
 
 app.use(bodyParser.json());
 app.listen(8000)
